d958b401642dc40492bae8ff58ce9998194b7436
Moritz <hi@mgross.dev>
2025-12-24 05:09:07 +0100

    make stuff prettier

--- components/header.js
+++ components/header.js
@@ -6,8 +6,10 @@ document.currentScript.insertAdjacentHTML('afterend', `
 <header>
     <div class="header-left">
         <h3>Moritz Groß</h3>
-        <a href="${pathPrefix}index.html">Home</a>
-        <a href="${pathPrefix}blog.html">Blog</a>
+        <nav>
+            <a href="${pathPrefix}index.html">Home</a>
+            <a href="${pathPrefix}blog.html">Blog</a>
+        </nav>
     </div>
     ${isHome ? `<img src="${pathPrefix}portrait.jpg" alt="Portrait of Moritz Groß" class="portrait">` : ''}
 </header>
