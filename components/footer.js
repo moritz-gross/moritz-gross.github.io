a81d8a2df08f0e51ebed667b6fd359aae6fb386b
Moritz <hi@mgross.dev>
2025-12-24 07:23:16 +0100

    add twitter

--- components/footer.js
+++ components/footer.js
@@ -6,6 +6,7 @@ document.currentScript.insertAdjacentHTML('afterend', `
             <a href="mailto:hi@mgross.dev">Email</a>
             <a href="https://github.com/moritz-gross" target="_blank" rel="noopener noreferrer">GitHub</a>
             <a href="https://linkedin.com/in/moritz-gross" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://x.com/Moritz__Gross" target="_blank" rel="noopener noreferrer">Twitter/X</a>
         </div>
     </section>
 </footer>
