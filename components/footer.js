bd27f4ab9066637d3f32edfbf3fb472532e94080
Moritz <hi@mgross.dev>
2025-12-24 07:55:29 +0100

    add StackOverflow

--- components/footer.js
+++ components/footer.js
@@ -7,6 +7,7 @@ document.currentScript.insertAdjacentHTML('afterend', `
             <a href="https://github.com/moritz-gross" target="_blank" rel="noopener noreferrer">GitHub</a>
             <a href="https://linkedin.com/in/moritz-gross" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             <a href="https://x.com/Moritz__Gross" target="_blank" rel="noopener noreferrer">Twitter/X</a>
+            <a href="https://stackoverflow.com/users/9727674/moritz-gro%C3%9F" target="_blank" rel="noopener noreferrer">StackOverflow</a>
         </div>
     </section>
 </footer>
